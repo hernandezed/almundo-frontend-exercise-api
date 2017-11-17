bd1800e39b8826fd1c20d1a94102aab4a1dd7081
Eduardo <hernandezed.1991@gmail.com>
2017-11-17 00:21:54 -0300

    arreglo script para dev

--- seeders/create-hotels.js
+++ seeders/create-hotels.js
@@ -1,5 +1,5 @@
 var data = require('./data/data');
-
+const fs = require('fs');
 data.forEach(function (hotel) {
     hotel.amenities = hotel.amenities.toString();
     hotel.id = parseInt(hotel.id);
@@ -7,17 +7,19 @@ data.forEach(function (hotel) {
 
 module.exports = {
     up: function (queryInterface, Sequelize) {
-        queryInterface.createTable('Hotels', {
-            id: {type: Sequelize.INTEGER, primaryKey: true,
-                autoIncrement: true,
-                allowNull: false},
-            name: Sequelize.STRING,
-            image: Sequelize.STRING,
-            stars: Sequelize.INTEGER,
-            price: Sequelize.DOUBLE(6, 2),
-            amenities: Sequelize.STRING
+        fs.unlink("db.development.sqlite", function () {
+            queryInterface.createTable('Hotels', {
+                id: {type: Sequelize.INTEGER, primaryKey: true,
+                    autoIncrement: true,
+                    allowNull: false},
+                name: Sequelize.STRING,
+                image: Sequelize.STRING,
+                stars: Sequelize.INTEGER,
+                price: Sequelize.DOUBLE(6, 2),
+                amenities: Sequelize.STRING
+            });
+            return queryInterface.bulkInsert('Hotels', data);
         });
-        return queryInterface.bulkInsert('Hotels', data);
     },
     down: function (queryInterface, Sequelize) {
         return queryInterface
