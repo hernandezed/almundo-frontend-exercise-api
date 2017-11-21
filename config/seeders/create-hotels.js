f1449f94e225f02eae483c030b214b8aac466bab
Eduardo <hernandezed.1991@gmail.com>
2017-11-21 16:45:43 -0300

    arreglo seeder

--- config/seeders/create-hotels.js
+++ config/seeders/create-hotels.js
@@ -1,5 +1,4 @@
 var data = require('./data/data');
-const fs = require('fs');
 data.forEach(function (hotel) {
     hotel.amenities = hotel.amenities.toString();
     hotel.id = parseInt(hotel.id);
@@ -7,19 +6,19 @@ data.forEach(function (hotel) {
 
 module.exports = {
     up: function (queryInterface, Sequelize) {
-        fs.unlink("db.development.sqlite", function () {
-            queryInterface.createTable('Hotels', {
-                id: {type: Sequelize.INTEGER, primaryKey: true,
-                    autoIncrement: true,
-                    allowNull: false},
-                name: Sequelize.STRING,
-                image: Sequelize.STRING,
-                stars: Sequelize.INTEGER,
-                price: Sequelize.DOUBLE(6, 2),
-                amenities: Sequelize.STRING
-            });
-            return queryInterface.bulkInsert('Hotels', data);
+        queryInterface.dropTable('Hotels');
+        queryInterface.createTable('Hotels', {
+            id: {type: Sequelize.INTEGER, primaryKey: true,
+                autoIncrement: true,
+                allowNull: false},
+            name: Sequelize.STRING,
+            image: Sequelize.STRING,
+            stars: Sequelize.INTEGER,
+            price: Sequelize.DOUBLE(6, 2),
+            amenities: Sequelize.STRING
         });
+        console.log("asdasd")
+        return queryInterface.bulkInsert('Hotels', data);
     },
     down: function (queryInterface, Sequelize) {
         return queryInterface
